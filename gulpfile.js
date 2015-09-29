0ead2951135fdf86cf7db34f0d5d10aef9625437
Nick Jurgens <nick@njurgens.net>
2015-09-29 18:15:22 -0400

    Add Jasmine test framework
    Jasmine tests are located under src/tests/.  Two tasks have been added
    to the gulpfile for building and running tests.
    
      test:
        Run all tests in dist/tests/.
    
      watch:
        When a file changes, run all tests in dist/tests/.
    
    The 'watch' test has been changed so that it does not itself build all
    files when it starts.  Instead it delegates to the 'build' task for an
    initial build and only re-builds files that change.
    
    Closes #2.

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,14 @@ var babel = require('gulp-babel'),
     debug = require('gulp-debug'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
+    jasmine = require('gulp-jasmine'),
     plumber = require('gulp-plumber'),
     watch = require('gulp-watch');
 
 /*
  * Gulp tasks
  */
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'test']);
 
 gulp.task('build', function() {
     return gulp.src('src/**/*.js')
@@ -26,15 +27,25 @@ gulp.task('build', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
-    return gulp.src('src/**/*.js')
-        .pipe(plumber(handle_compiler_error))
-        .pipe(watch('src/**/*.js'))
-        .pipe(babel({
-            optional: ['runtime']
-        }))
-        .pipe(debug({title: 'watch'}))
-        .pipe(gulp.dest('dist'));
+gulp.task('watch', ['build'], function() {
+    // when a file changes, re-run all tests
+    gulp.watch('dist/**/*.js', ['test']);
+
+    return watch('src/**/*.js', function(vinyl) {
+        gulp.src(vinyl.path)
+            .pipe(plumber(handle_compiler_error))
+            .pipe(babel({
+                optional: ['runtime']
+            }))
+            .pipe(debug({title: 'watch'}))
+            .pipe(gulp.dest('dist'));
+    });
+});
+
+gulp.task('test', function(cb) {
+    return gulp.src('dist/tests/**/*.js')
+        .pipe(jasmine())
+        .pipe(debug({title: 'jasmine'}));
 });
 
 function handle_compiler_error(err) {
---
+++ src/tests/example-test.js
@@ -0,0 +1,13 @@
+/*
+ * Test Jasmine suite.
+ *
+ * TODO: remove this once there are actual tests
+ *
+ * Copyright (c) 2015 Nick Jurgens <nicholas2010081@gmail.com>
+ */
+
+describe('foo test', function() {
+	it('asserts true', function() {
+		expect(true).toBe(true);
+	});
+});
