b05583318d9cec3f891456093c5d34450b4027d6
Lithas <lithas@umich.edu>
2015-09-29 16:25:32 -0400

    Stop being rude :(

--- src/commands/marco.js
+++ src/commands/marco.js
@@ -2,11 +2,5 @@
  * Copyright (c) 2015 Nick Jurgens <nicholas2010081@gmail.com>
  */
 export default function(text, message) {
-    console.dir(message);
-
-    if (message.from.id === 69676364) {
-        return 'polo';
-    }
-
-    return 'pong';
+    return 'polo';
 }
