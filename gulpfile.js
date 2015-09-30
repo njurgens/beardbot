870438d7edd01343caed9b8899451631b2b1edc4
Nick Jurgens <nick@njurgens.net>
2015-09-29 23:41:48 -0400

    Fix issue with gulp watch placing all files in dist/

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var babel = require('gulp-babel'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
     jasmine = require('gulp-jasmine'),
+    path = require('path'),
     plumber = require('gulp-plumber'),
     watch = require('gulp-watch');
 
@@ -32,18 +33,22 @@ gulp.task('watch', ['build'], function() {
     gulp.watch('dist/**/*.js', ['test']);
 
     return watch('src/**/*.js', function(vinyl) {
+        var dest = path.dirname(vinyl.path.replace(
+                path.join(vinyl.cwd, vinyl.base), 'dist'));
+
         gulp.src(vinyl.path)
             .pipe(plumber(handle_compiler_error))
             .pipe(babel({
                 optional: ['runtime']
             }))
             .pipe(debug({title: 'watch'}))
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest(dest));
     });
 });
 
 gulp.task('test', function(cb) {
     return gulp.src('dist/tests/**/*.js')
+        .pipe(plumber(handle_jasmine_error))
         .pipe(jasmine())
         .pipe(debug({title: 'jasmine'}));
 });
@@ -51,4 +56,9 @@ gulp.task('test', function(cb) {
 function handle_compiler_error(err) {
     gutil.log(err.plugin, gutil.colors.red(err.name), '\n', err.message,
             '\n\n' + err.codeFrame, '\n');
+}
+
+function handle_jasmine_error(err) {
+    gutil.log('jasmine', gutil.colors.red(err.name), err.message);
+    this.emit('end');
 }
